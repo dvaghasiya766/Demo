8c0466b4e5a940a4cb390a451e9a5b81a71c79c7
dvaghasiya766 <dvaghasiya766@rku.ac.in>
2025-03-16 18:54:49 +0530

    set

---
+++ Assets/JavaScript/CustomJSProgram1001.js
@@ -0,0 +1,28 @@
+// Load header
+fetch("Links.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to load header: " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading header:", error);
+  });
+// Load Footer
+fetch("Footer.html")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to load header: " + response.statusText);
+    }
+    return response.text();
+  })
+  .then((data) => {
+    document.getElementById("header").innerHTML = data;
+  })
+  .catch((error) => {
+    console.error("Error loading header:", error);
+  });
